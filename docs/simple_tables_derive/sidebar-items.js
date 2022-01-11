38d05a4b620cbbaeb4452cee809b451ad78e181b
Jomy10 <jonas.vbs4@gmail.com>
2022-01-11 22:48:32 +0100

    Pages

---
+++ docs/simple_tables_derive/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"attr":[["table","Initialises a struct to be a Table that holds information about a table row."],["table_row","Initialises a struct to be used as a TableRow so it can be used as an entry inside of a Table"]]});
